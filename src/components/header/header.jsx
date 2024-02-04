8568a23f36614d3d7618a61306cd4b6f9b4b1765
Derek_yip <derek.yipchinsan@gmail.com>
2024-02-04 14:44:27 +0800

    add environment variables and remove

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,21 +3,23 @@ import { FaGithub, FaLinkedin, FaHeart } from "react-icons/fa";
 import "./header.css";
 
 function Header() {
+  const GITHUB_LINK = import.meta.env.VITE_REACT_GITHUB_LINK;
+  const LINKEDIN_LINK = import.meta.env.VITE_REACT_LINKEDIN_LINK;
+  const PAYME_LINK = import.meta.env.VITE_REACT_PAYME_LINK;
+
   return (
     <header className="header">
       <nav className="navbar">
-        <p className="title">
-          Derek Yip's Project - Passport Photo Cropper
-        </p>
+        <p className="title">Derek Yip's Project - Passport Photo Cropper</p>
         <div className="social-icons">
-          <a href="https://github.com/derek-yip" target="_blank">
-            <FaGithub className="icon github-icon" alt="GitHub"/>
+          <a href={GITHUB_LINK} target="_blank">
+            <FaGithub className="icon github-icon" alt="GitHub" />
           </a>
-          <a href="https://www.linkedin.com/in/derekyip-74272921b" target="_blank">
-            <FaLinkedin className="icon linkedIn-icon"  alt="LinkedIn"/>
+          <a href={LINKEDIN_LINK} target="_blank">
+            <FaLinkedin className="icon linkedIn-icon" alt="LinkedIn" />
           </a>
-          <a href="https://www.buymeacoffee.com/derekyip" target="_blank">
-            <FaHeart className="icon heart-icon"  alt="Sponsor a Coffee"/>
+          <a href={PAYME_LINK} target="_blank">
+            <FaHeart className="icon heart-icon" alt="Sponsor a Coffee" />
           </a>
         </div>
       </nav>
